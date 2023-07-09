2a5fb4a0e5dc9be7f5a5bf798b1379081857ef5b
Yuliya_K <live2do@yandex.ru>
2023-07-10 00:52:59 +0300

    add Bandit

---
+++ Bandit.java
@@ -0,0 +1,29 @@
+// Класс Разбойник
+public class Bandit extends Soldier{
+    
+    private int blade; // ножи
+
+    public static String setName(){
+        return String.format("Hero_Bandit #%d", ++Killer.number);
+    }
+
+    public Bandit() {
+        super(setName());
+        this.blade = Bandit.r.nextInt(0, 100);
+    }
+
+    public String getInfo() {
+        return String.format("%s  Speed: %d  Blade: %d", 
+            super.getInfo(), super.speed, this.blade);
+    }
+
+    // Метод ограбление
+    public void takeMoney(int hour, AbsoluteHero target) {
+        super.money += hour * 100;
+        super.hunger += hour * 30;
+        if (hunger > 100) super.die();
+        super.speed -= hour * 10;
+        this.blade -= hour * 5;
+        target.moneyOff(hour*100);
+    }
+}
